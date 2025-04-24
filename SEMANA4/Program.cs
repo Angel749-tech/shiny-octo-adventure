1fb1f8592a98f51721b785e91fc21911af4068a6
angelo <sebita_as7@hotmail.com>
2025-04-24 11:01:55 -0500

    AVANCE DOS PRESENCIAL

--- SEMANA4/Program.cs
+++ SEMANA4/Program.cs
@@ -13,9 +13,9 @@ namespace SEMANA4
         static void Main(string[] args)
         {
 
-            Ejercicio4();
+            ejercicio7();
             Console.ReadKey();
-          
+
         }
 
 
@@ -24,7 +24,7 @@ namespace SEMANA4
 
             int num1;
             Console.WriteLine("Ingresa un numero");
-            num1= int.Parse(Console.ReadLine());
+            num1 = int.Parse(Console.ReadLine());
 
             if (num1 % 2 == 0)
             {
@@ -42,19 +42,19 @@ namespace SEMANA4
         {
             int edad;
             Console.WriteLine("ingrese una edad");
-            edad=int.Parse(Console.ReadLine());
+            edad = int.Parse(Console.ReadLine());
 
-           if (edad < 0)
+            if (edad < 0)
             {
                 Console.WriteLine("Edad invalida");
 
             }
-           else if (edad >= 18)
+            else if (edad >= 18)
             {
                 Console.WriteLine("Es mayor de edad");
 
             }
-           else
+            else
             {
                 Console.WriteLine("Es menor de edad");
             }
@@ -111,17 +111,17 @@ namespace SEMANA4
             {
                 Console.WriteLine("Angulo obtuso");
             }
-            else 
+            else
                 if (angulo == 180)
             {
                 Console.WriteLine("Ángulo llano");
             }
-            else 
-                if (angulo > 180 && angulo<360)
+            else
+                if (angulo > 180 && angulo < 360)
             {
                 Console.WriteLine("Ángulo cóncavo");
             }
-            else 
+            else
                 if (angulo == 360)
             {
                 Console.WriteLine("Ángulo completo");
@@ -131,17 +131,17 @@ namespace SEMANA4
             Console.ReadKey();
         }
 
-        static void Ejercicio4()
+        static void Ejercicio5()
         {
             // Dado el tipo de chocolate y la cantidad de unidades adquiridas,
             // diseñe un programa que determine el importe de la compra, el importe
             // del descuento, el importe a pagar y la cantidad de caramelos de obsequio.
 
 
-            double tipochocolate, preciounitario=0, cantichocolate, descuento, importebruto, importedescuento, importefinal, regalo=0;
+            double tipochocolate, preciounitario = 0, cantichocolate, descuento, importebruto, importedescuento, importefinal, regalo = 0;
 
             Console.WriteLine("Seleccione un tipo de chocolate por el número: Primor (1), Dulzura (2), Tentación (3), Explosión (4)");
-            tipochocolate=double.Parse(Console.ReadLine());
+            tipochocolate = double.Parse(Console.ReadLine());
             Console.WriteLine("Digitar la cantidad de chocolates");
             cantichocolate = double.Parse(Console.ReadLine());
 
@@ -150,7 +150,7 @@ namespace SEMANA4
                 case 1:
                     Console.WriteLine("Primor seleccionado");
                     preciounitario = 8.5;
-                break;
+                    break;
 
                 case 2:
                     Console.WriteLine("Dulzura seleccionado");
@@ -169,7 +169,7 @@ namespace SEMANA4
 
                 default:
                     Console.WriteLine("Tipo de chocolate inválido");
-                break;
+                    break;
             }
 
             importebruto = preciounitario * cantichocolate;
@@ -197,7 +197,7 @@ namespace SEMANA4
 
             importefinal = importebruto - importedescuento;
 
-            if (importefinal>=250)
+            if (importefinal >= 250)
             {
                 regalo = cantichocolate * 3;
             }
@@ -214,6 +214,220 @@ namespace SEMANA4
 
             Console.WriteLine("Fin de la compra, ¡vuelva pronto =D !");
             Console.ReadKey();
+        }
+
+        static void Ejercicio6()
+        {
+            // Algoritmo que retorne temperatura alta, si el ingreso es mayor o igual a 37
+
+            double temp;
+
+            Console.WriteLine("Digitar temperatura");
+            temp = double.Parse(Console.ReadLine());
+
+            if (temp >= 37)
+            {
+                Console.WriteLine("TEMPERATURA ALTA; RETORNANDO!!!!!!.....");
+            }
+            else
+            {
+                Console.WriteLine("Temperatura normal carajo");
+            }
+            Console.WriteLine("Gracias por usar el medidor de temperatura, presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+
+        static void ejercicio7()
+        {
+            double ruta, calidad, descuento, importe, cantidad, importedescuento;
+
+            Console.WriteLine("Seleccionar ruta");
+            Console.WriteLine("Rutas disponibles: Lima a Huanuco (1) - Lima a Huancayo (2)");
+            ruta = double.Parse(Console.ReadLine());
+
+
+
+            switch (ruta)
+            {
+                case 1:
+                    Console.WriteLine("Seleccionar calidad");
+                    Console.WriteLine("Calidad A (1) - Calidad B (2) - Calidad (3)");
+                    calidad = double.Parse(Console.ReadLine());
+
+                    if (calidad == 1)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+                        if (cantidad > 4)
+                        {
+                            importe = cantidad * 45;
+                            descuento = importe * 0.05;
+                            importedescuento = importe - descuento;
+                            Console.WriteLine($"El importe de la compra es S/. {importe}, el importe del descuento es S/. {descuento} y el importe a pagar es {importedescuento}");
+                            Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            importe = cantidad * 45;
+                            Console.WriteLine($"El importe a pagar es: " + importe);
+                            Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                            Console.ReadKey();
+                        }
+                    }
+
+                    if (calidad == 2)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+
+                        importe = cantidad * 35;
+                        Console.WriteLine($"El importe a pagar es: " + importe);
+                        Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                        Console.ReadKey();
+
+                    }
+
+                    if (calidad == 3)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+
+                        importe = cantidad * 30;
+                        Console.WriteLine($"El importe a pagar es: " + importe);
+                        Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                        Console.ReadKey();
+
+                    }
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Seleccionar calidad");
+                    Console.WriteLine("Calidad A (1) - Calidad B (2) - Calidad (3)");
+                    calidad = double.Parse(Console.ReadLine());
+
+                    if (calidad == 1)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+                        if (cantidad > 4)
+                        {
+                            importe = cantidad * 38;
+                            descuento = importe * 0.05;
+                            importedescuento = importe - descuento;
+                            Console.WriteLine($"El importe de la compra es S/. {importe}, el importe del descuento es S/. {descuento} y el importe a pagar es {importedescuento}");
+                            Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            importe = cantidad * 38;
+                            Console.WriteLine($"El importe a pagar es: " + importe);
+                            Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                            Console.ReadKey();
+                        }
+                    }
+
+                    if (calidad == 2)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+
+                        importe = cantidad * 33;
+                        Console.WriteLine($"El importe a pagar es: " + importe);
+                        Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                        Console.ReadKey();
+
+                    }
+
+                    if (calidad == 3)
+                    {
+                        Console.WriteLine("Digitar cantidad de boletos");
+                        cantidad = double.Parse(Console.ReadLine());
+
+                        importe = cantidad * 28;
+                        Console.WriteLine($"El importe a pagar es: " + importe);
+                        Console.WriteLine("Gracias por su compra. Presione cualquier tecla para salir");
+                        Console.ReadKey();
+
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Número inválido, reiniciar el sistema");
+                    break;
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+        static void ejercicio8()
+        {
+            // Algoritmo que determine la condición de un alumno, si
+            // su nota es mayor o igual a 13 "aprobado", caso contrario "desaprobado"
+
+            double nota;
+
+            Console.WriteLine("Digitar Nota");
+            nota = double.Parse(Console.ReadLine());
+            if (nota >= 0 && nota <= 20)
+                if (nota >= 13)
+                {
+                    Console.WriteLine("Nota aprobatoria");
+                }
+                else
+                {
+                    Console.WriteLine("Nota desaprobatoria");
+                }
+
+        }
+
+        static void ejercicio9()
+        {
+            double n1, n2, n3;
+            Console.WriteLine("Digitar tres números");
+            n1 = double.Parse(Console.ReadLine());
+            n2 = double.Parse(Console.ReadLine());
+            n3 = double.Parse(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("El primer número es mayor");
+            }
+            else
+            if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("El segundo número es mayor");
+            }
+            else
+            if (n3 > n1 && n3 > n2)
+            {
+                Console.WriteLine("El tercer número es mayor");
+            }
+
+        }
+
+        static void ejercicio10()
+        {
+            double sueldo, comisión, ventas, sueldofinal;
+            Console.WriteLine("Ingresar ventas");
+            ventas = double.Parse(Console.ReadLine());
+            sueldo = 930;
+            comisión = ventas * 0.10;
+            sueldofinal = comisión + sueldo;
+            Console.WriteLine($"El sueldo de un vendedor es S/. {sueldo}, la comisión de sus ventas es S/. {comisión} y su sueldo neto calculado es: {sueldofinal}");
+
         }
 
     }
