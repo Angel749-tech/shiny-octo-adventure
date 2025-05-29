f27682a0122f3bcc61aea44b04b892e3b6a01cc4
angelo <sebita_as7@hotmail.com>
2025-05-29 11:58:10 -0500

    CLASE 10 29-05-2025

---
+++ Semana 10/Cajero.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_10
+{
+    class Cajero
+    {
+        private double saldo = 1000;
+
+        public double consultar()
+        {
+            return saldo;
+        }
+
+        public void deposito(double monto)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese el monto a depositar: ");
+                string entrada = Console.ReadLine();
+                if (double.TryParse(entrada, out monto))
+                {
+                    if (monto > 0)
+                    {
+                        saldo += monto;
+                        Console.WriteLine("Deposito Exitoso");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("ERROR: Solo se pueden depositar números mayores a 0. ");
+                }
+                else
+                {
+                    Console.WriteLine("¡Error, solo se permiten números!");
+                }
+
+            } while (true);
+        }
+
+        public void retiro(double monto)
+        {
+            do
+            {
+                Console.WriteLine("Cuanto decea retirar");
+                string entrada = Console.ReadLine();
+                if (double.TryParse(entrada, out monto))
+                {
+                    if (monto > saldo)
+                    {
+                        Console.WriteLine("ERRROR404: El monto no puede ser mayor al saldo");
+
+                    }
+                    else if (monto < 0)
+                        Console.WriteLine("ERROR: Solo se pueden retirar números mayores a 0...");
+                    else
+                    {
+                        saldo -= monto;
+                        Console.WriteLine("Retiro Exitoso, saldo actual: S/. " + saldo);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("¡Error, solo se permiten números!");
+                }
+
+            } while (true);
+        }
+    }
+}
---
+++ Semana 10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_10
+{
+    class Program
+    {
+        double saldo = 1000;
+        static void Main(string[] args)
+        {
+            char opcion, continuar;
+            Cajero c = new Cajero();
+            
+            do
+            {
+
+                do
+                {
+                    Console.WriteLine("Bievenidos al sistemas de cajeros\n");
+                    Console.WriteLine("1. Consultar saldo");
+                    Console.WriteLine("2. Depositar saldo");
+                    Console.WriteLine("3. Retirar saldo  ");
+                    Console.WriteLine("4. Salir");
+
+                    Console.Write("Ingrese una opción: \n");
+                    opcion = Console.ReadKey().KeyChar;
+                    Console.Clear();
+                }
+                while (opcion < '1' || opcion > '4');
+
+                switch (opcion)
+                {
+                    case '1':
+                        Console.WriteLine("Su saldo actual es: S/. " + c.consultar());
+
+                        break;
+                    case '2':
+
+                        c.deposito(0);
+
+                        break;
+                    case '3':
+
+                        c.retiro(0);
+
+                        break;
+
+                    case '4':
+
+                        Console.WriteLine("¡Gracias por usar el programa!");
+                        return;
+                }
+
+                do
+                {
+
+                    Console.WriteLine("\n¿Desea regresar al menú S/N: ?");
+                    continuar = char.ToUpper(Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+                    if (continuar != 'S' && continuar != 'N')
+                    {
+                        Console.WriteLine("¡Opción inválida! Ingrese solo S o N: ");
+                    }
+
+                }
+                while (continuar != 'S' && continuar != 'N');
+                Console.Clear();
+            } while (continuar == 'S');
+
+        }
+    }
+}
