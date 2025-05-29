f073cd5d12ae3ee76eaa70fd3ab4f954ca6721bd
angelo <sebita_as7@hotmail.com>
2025-05-29 12:00:45 -0500

    CLASE 10 con T2 29-05-2025

---
+++ T2/Alvarado Saravia Angel Sebasthiam/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alvarado_Saravia_Angel_Sebasthiam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n2 = 3, cantidaddenumeros = 0, sumadenumeros = 0, promediodelosnumeros, mayordelosnumeros = 0;
+            // Desarrolle un programa que muestre los múltiplos de 3 a partir del 3 hasta el número que el usuario ingrese en la consola.
+            Console.WriteLine("Por favor, digite un número entero a partir del 3 en adelante para mostrar los múltiplos del tres hasta el susodicho: \n");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 3)
+            {
+                Console.WriteLine("Error, no se puede calcular los múltiplos de 3");
+            }
+            else
+                while (n2 <= n)
+                {
+
+                    if (n2 % 3 == 0)
+                    {
+                        Console.WriteLine("Multiplo de 3: ");
+                        Console.WriteLine(n2);
+                        
+                        cantidaddenumeros++;
+                        sumadenumeros += n2;
+                    }
+                    n2++;
+
+                }
+            if (n % 3 == 0)
+            {
+                mayordelosnumeros = +n;
+            }
+            else
+            {
+
+            }
+
+            promediodelosnumeros = ((sumadenumeros) / cantidaddenumeros);
+            Console.WriteLine("\nAdemás: \n");
+            Console.WriteLine("La cantidad de los numeros son: " + cantidaddenumeros);
+            Console.WriteLine("La suma de los números son: " + sumadenumeros);
+            Console.WriteLine("El promedio de los números son: " + promediodelosnumeros);
+            Console.WriteLine("El mayor de los números es " + (mayordelosnumeros));
+        }
+    }
+}
---
+++ T2/AlvaradoSaravia_02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlvaradoSaravia_02
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Dados el tipo de habitación
+            // y el número de días de alojamiento de un cliente,
+            // diseñe una función que calcule y retorne el importe de compra.
+
+            double tipodehabitacion, diasalojamiento, importedecompra=0;
+
+            Console.WriteLine("Seleccione un tipo de habitación: \n");
+            Console.WriteLine("1. Simple");
+            Console.WriteLine("2. Matrimonial: ");
+            Console.WriteLine("3. Doble ");
+            Console.WriteLine("4. Triple: \n");
+
+            tipodehabitacion = double.Parse(Console.ReadLine());
+
+            if (tipodehabitacion <= 0 & tipodehabitacion <= 0)
+            {
+                Console.WriteLine("Digitar un número válido por favor");
+                return;
+            }
+
+            Console.WriteLine("Digitar cantidad de días de alojamiento: \n");
+            diasalojamiento = double.Parse(Console.ReadLine());
+
+            if (diasalojamiento <= 0 & tipodehabitacion <= 0)
+            {
+                Console.WriteLine("Digitar un número válido por favor");
+            }
+            else 
+            {
+                importedecompra = importe(tipodehabitacion, diasalojamiento);
+                Console.WriteLine($"El importe a pagar es de: {+importedecompra} soles");
+            } 
+
+        }
+
+        static double importe (double tipodehabitacion, double diasalojamiento)
+        {
+            double precio = 0;
+            switch (tipodehabitacion)
+            {
+                
+                case '1':
+
+                    precio = 80;
+                    break;
+                case '2':
+                    precio = 160;
+
+                    break;
+                case '3':
+
+                    precio = 200;
+                    break;
+                case '4':
+
+                    precio = 240;
+                    break;
+
+            }
+            return precio * diasalojamiento;
+        } 
+
+    }
+}
