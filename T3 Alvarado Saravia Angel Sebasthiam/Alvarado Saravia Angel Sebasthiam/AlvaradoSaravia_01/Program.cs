92c255515aba87b957c601f3a87776dbaa5b1e74
angelo <sebita_as7@hotmail.com>
2025-06-19 12:08:42 -0500

    PRACTICA SEMANA 13.1

---
+++ T3 Alvarado Saravia Angel Sebasthiam/Alvarado Saravia Angel Sebasthiam/AlvaradoSaravia_01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlvaradoSaravia_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] serie = { 45, 43, 41, 39, 37, 4, 18, 20, 40, 5, 9, 25, 99, 14, 64};
+            int[] serie2 = { 3, 6, 12, 24, 48, 2, 6, 10, 4, 5, 3, 5, 11, 2, 8};
+            double[] serie3 = { 15, 7.16, 3.416, 1.625, 0.77083, 2, 3, 2, 10,1, 3, 5, 9, 7, 9};
+            // Función que imprime la serie
+            Console.WriteLine("* La serie es: "); imprimeserieALVARADOSARAVIA(serie,serie2);
+            // Función que suma la serie
+            Console.WriteLine("* La suma de la serie es: " + sumaserieALVARADOSARAVIA(serie3));
+        }
+
+        static void imprimeserieALVARADOSARAVIA(int[] serie, int[] serie2)
+        {
+            for (int i = 0; i < serie.Length; i++)
+            {
+                Console.WriteLine(serie[i] + "/" + serie2[i]);
+            }
+
+        }
+
+        static double sumaserieALVARADOSARAVIA(double[] serie3)
+        {
+            double suma = 0;
+            for (int i = 0; i < serie3.Length; i++)
+            {
+                suma += serie3[i];
+            }
+
+            return suma;
+        }
+
+    }
+}
---
+++ T3 Alvarado Saravia Angel Sebasthiam/Alvarado Saravia Angel Sebasthiam/AlvaradoSaravia_02/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlvaradoSaravia_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///// --- Una Empresa comercial le solicita que implemente un arreglo de enteros --
+            int[] arreglo = new int[6];
+            // Una función que llene el arreglo con valores aleatorios (Entre 1 y 500)
+            valoresaleatoriosALVARADOSARAVIA(arreglo);
+            // Una función que muestre en consola los datos del arreglo
+            Console.WriteLine("Datos del arreglo: "); datosdelarregloALVARADOSARAVIA(arreglo);
+            // Una función que calcule y retorne la posición del tercer número par
+            Console.Write("\nLa posición del tercer número par es (-1 si no existe): "); posiciontercernumeroparALVARADOSARAVIA(arreglo);
+            // Una función que calcule y retorne el antepenúltimo múltiplo de 5
+            Console.WriteLine("\nEl antepenultimo multiplo de 5 es (-1 si no existe): " + multiplo5ALVARADOSARAVIA(arreglo));
+
+        }
+
+        static void valoresaleatoriosALVARADOSARAVIA(int[] arreglo)
+        {
+            Random aleatorio = new Random();
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = aleatorio.Next(1, 500+1);
+            }
+        }
+        static void datosdelarregloALVARADOSARAVIA(int[] arreglo)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(arreglo[i] + ",");
+            }
+        }
+        static int posiciontercernumeroparALVARADOSARAVIA(int[] arreglo)
+        {
+            int par = 0;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] % 2 == 0)
+                {
+                    par++;
+                    if (par == 3)
+                        Console.Write(arreglo[i]);
+                }
+            }
+            return -1;
+
+        }
+
+        static int multiplo5ALVARADOSARAVIA(int[] arreglo)
+        {
+            int multiplo = 0;
+            for (int i = arreglo.Length-1; i >= 0; i--)
+            {
+                if (arreglo[i] % 5 == 0)
+                {
+                    multiplo++;
+                    if (multiplo == 3)
+                        Console.Write(arreglo[i]);
+                }
+        
+            }
+            return -1; // Si no existe
+        }
+    }
+}
