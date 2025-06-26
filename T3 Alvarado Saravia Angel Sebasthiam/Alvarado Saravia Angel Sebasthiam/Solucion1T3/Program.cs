f18990452c6954a05f9ddd77220e370cad91ec8f
Sebas <sebas@gmail.com>
2025-06-26 05:12:41 -0500

    practica

---
+++ T3 Alvarado Saravia Angel Sebasthiam/Alvarado Saravia Angel Sebasthiam/Solucion1T3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion1T3
+{
+    internal class Program
+    {
+        static double num = 45, dem = 3, suma = 0;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Para la siguiente sucesión: ");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(num + "/" + dem + " = " + num / dem);
+                num -= 2;
+                dem *= 2;
+            }
+            suma = SUMA(num, dem);
+            Console.WriteLine("La suma de toda la sucesión es: " + suma.ToString("F4"));
+            Console.WriteLine("La suma de toda la sucesión es: " + Math.Round(suma, 2));
+            Console.WriteLine("La suma de toda la sucesión es: " + suma);
+
+        }
+
+        static double SUMA(double num, double dem)
+        {
+            double nuM = 45, deM = 3, suma = 0;
+            for (int i = 0;i < 15; i++)
+            {
+                suma += nuM / deM; 
+                nuM -= 2;
+                deM *= 2;
+            }
+
+            return suma; 
+        }
+    }
+}
---
+++ T3 Alvarado Saravia Angel Sebasthiam/Alvarado Saravia Angel Sebasthiam/Solucion2T3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion2T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] arreglo = new int[6];
+
+            // Función que llena el arreglo
+            lenarFunciones(arreglo);
+            // Imprimir arreglo
+            Console.WriteLine("Datos del arreglo: "); imprimirfunciones(arreglo);
+            // Antepenúltimo múltiplo de 5
+            int antepenultimo=antepenultimoquintuploFunciones(arreglo);
+            Console.WriteLine("\nAntepenúltimo múltiplo de 5: " + antepenultimo);
+            // Tercer número par
+            int postercerpar= posiciontercernumeroparALVARADOSARAVIA(arreglo);
+            Console.WriteLine("\nTercer número par: " + postercerpar);
+        }
+
+        static void lenarFunciones(int[] arreglo)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = random.Next(0, 500 + 1);
+            }
+        }
+
+        static void imprimirfunciones(int[] arreglo)
+        {
+            for (int i = 0;i < arreglo.Length; i++)
+            {
+                Console.Write(arreglo[i] + ", ");
+            }
+        }
+
+        static int antepenultimoquintuploFunciones(int[] arreglo)
+        {
+            int sum = 0;
+            for (int i = arreglo.Length - 1; i >= 0; i--)
+            {
+                if (arreglo[i] % 5 == 0)
+                {
+                    sum ++;
+                    if (sum == 3)
+                    {
+                        return arreglo[i];
+                    }
+                }
+            }
+            return -1; // Si no existe
+        }
+
+        static int posiciontercernumeroparALVARADOSARAVIA(int[] arreglo)
+        {
+            int num = 0;
+            for(int i = 0; i < 6; i++)
+            {
+                if (arreglo[i] % 2 == 0)
+                {
+                    num ++;
+                    if (num == 3)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
